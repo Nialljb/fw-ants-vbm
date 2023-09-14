0e667b20c5d82d908026282b6047e07261c4a1e1
nialljb <nialljbourke@gmail.com>
2023-09-14 11:23:49 +0100

    upate ROI subprocess

--- utils/ROI.py
+++ utils/ROI.py
@@ -20,12 +20,12 @@ def run_jolly(FLYWHEEL_BASE, WORK, OUTPUT_DIR, studyBrainReference, wm, wm_mask,
                 MNIAligned = (OUTPUT_DIR + "/" + regionName + "_Aligned.nii.gz")
                 # Run registration
                 # subprocess.run(["WarpImageMultiTransform 3 " + f + " " + MNIAligned + " -R " + studyBrainReference + " " + brainWarpField + " " + brainAffineField +" --use-BSpline"], shell=True, check=True)	
-                subprocess.run(['antsApplyTransforms -d 3 -i ' + f + ' -r ' + studyBrainReference + ' -t ' + MNI_WARP + ' -t ' + MNI_AFFINE + ' -n GenericLabel -o ' + MNIAligned])
+                subprocess.run(['antsApplyTransforms -d 3 -i ' + f + ' -r ' + studyBrainReference + ' -t ' + MNI_WARP + ' -t ' + MNI_AFFINE + ' -n GenericLabel -o ' + MNIAligned], shell=True, check=True)
                 subprocess.run(["fslmaths " + MNIAligned + " -mul " + wm_mask + " " + MNIAligned], shell=True, check=True)	
 
                 # Calculate volume
-                est = float(subprocess.check_output(["fslstats " + wm + " -k " + MNIAligned + " -M | awk '{print $1}' "],shell=True).decode("utf-8"))
-                mask_vol = float(subprocess.check_output(["fslstats " + wm + " -k " + MNIAligned + " -V | awk '{print $1}' "],shell=True).decode("utf-8"))
+                est = float(subprocess.check_output(["fslstats " + wm + " -k " + MNIAligned + " -M | awk '{print $1}' "], shell=True).decode("utf-8"))
+                mask_vol = float(subprocess.check_output(["fslstats " + wm + " -k " + MNIAligned + " -V | awk '{print $1}' "], shell=True).decode("utf-8"))
                 volume = int(est * mask_vol)
                 df[regionName] = volume
     return df
@@ -50,12 +50,12 @@ def run_subcortical(FLYWHEEL_BASE, WORK, OUTPUT_DIR, studyBrainReference, gm, gm
 
                 # Run registration
                 # subprocess.run(["WarpImageMultiTransform 3 " + f + " " + MNIAligned + " -R " + studyBrainReference + " " + brainWarpField + " " + brainAffineField +" --use-BSpline"], shell=True, check=True)	
-                subprocess.run(['antsApplyTransforms -d 3 -i ' + f + ' -r ' + studyBrainReference + ' -t ' + MNI_WARP + ' -t ' + MNI_AFFINE + ' -n GenericLabel -o ' + MNIAligned])
+                subprocess.run(['antsApplyTransforms -d 3 -i ' + f + ' -r ' + studyBrainReference + ' -t ' + MNI_WARP + ' -t ' + MNI_AFFINE + ' -n GenericLabel -o ' + MNIAligned], shell=True, check=True)
                 subprocess.run(["fslmaths " + MNIAligned + " -mul " + gm_mask + " " + MNIAligned], shell=True, check=True)	
             
                 # Calculate volume
-                est = float(subprocess.check_output(["fslstats " + gm + " -k " + MNIAligned + " -M | awk '{print $1}' "],shell=True).decode("utf-8"))
-                mask_vol = float(subprocess.check_output(["fslstats " + gm + " -k " + MNIAligned + " -V | awk '{print $1}' "],shell=True).decode("utf-8"))
+                est = float(subprocess.check_output(["fslstats " + gm + " -k " + MNIAligned + " -M | awk '{print $1}' "], shell=True).decode("utf-8"))
+                mask_vol = float(subprocess.check_output(["fslstats " + gm + " -k " + MNIAligned + " -V | awk '{print $1}' "], shell=True).decode("utf-8"))
                 volume = int(est * mask_vol)
                 df[regionName] = volume
     return df
