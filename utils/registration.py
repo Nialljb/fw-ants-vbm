53e30d89116e27e0bddb847e1e40270ebba496b0
nialljb <nialljbourke@gmail.com>
2023-09-14 14:09:12 +0100

    To - 2

--- utils/registration.py
+++ utils/registration.py
@@ -2,10 +2,10 @@ import subprocess
 
 def MNI2BCP(BCP, OUTPUT_DIR):
     MNI = '/flywheel/v0/app/templates/MNI152_T1_1mm_brain.nii.gz'
-    OUT = (OUTPUT_DIR + '/mniTobcp_')
+    OUT = (OUTPUT_DIR + '/mni2bcp_')
 
     print('BCP: ' + BCP)
     print('MNI: ' + MNI)
     print('OUT: ' + OUT)
-    subprocess.run(['antsRegistrationSyNQuick.sh -f ' + BCP + ' -m ' + MNI + ' -t s -o ' + OUT], shell=True, check=True)
+    subprocess.run(['antsRegistrationSyNQuick.sh -f ' + BCP + ' -m ' + MNI + ' -t s -o ' + OUT], shell=True, capture_output = True)
 
